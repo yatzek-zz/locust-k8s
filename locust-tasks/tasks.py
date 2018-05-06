08f5be14b44d83267299c8b0378e687b38f62c8e
Jacek Szlachta <jacek.szlachta@gmail.com>
2018-05-06 16:56:34 +0100

    simplified task

--- locust-tasks/tasks.py
+++ locust-tasks/tasks.py
@@ -22,3 +22,17 @@ class WebsiteUser(HttpLocust):
     task_set = UserBehavior
     min_wait = 5000
     max_wait = 9000
+
+# Single URL, no wait time
+# from locust import HttpLocust, TaskSet, task
+#
+# class UserBehavior(TaskSet):
+#
+#     @task(1)
+#     def index(self):
+#         self.client.get("/")
+#
+# class WebsiteUser(HttpLocust):
+#     task_set = UserBehavior
+#     min_wait = 0
+#     max_wait = 0
